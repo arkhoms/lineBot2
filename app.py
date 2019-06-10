2c6e490f9b1030e2c3d2e13a54284fc6da0b442a
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-06-10 18:44:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def index():
     return "สวัสดีครับ"
 
+@app.route("/webhook", methods=['GET', 'POST'])
+def webhook() :
+    if request.method == 'POST'"
+        reture 'OK"
+
 if __name__ == '__main__':
     app.run(debug=True)
