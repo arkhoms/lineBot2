d15ecb312227f094001a9e3d0a346a68e38333e0
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-06-10 18:51:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook() :
     if request.method == 'POST':
-        reture "OK"
+        return "OK"
 
 if __name__ == '__main__':
     app.run()
