faf1da6ab6bc2abeb01299743a8d4d2a0eef3c07
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-06-11 19:25:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,19 +13,19 @@ def webhook():
 
 @app.route('/callback', methods=['POST'])
 def callback():
-  json_line = request.get_json()
-  json_line = json.dumps(json_line)
-  decoded = json.loads(json_line)
-  user = decoded["events"][0]['replyToken']
-  #id=[d['replyToken'] for d in user][0]
-  #print(json_line)
-  print("ผู้ใช้：",user)
-  sendText(user,'งง') # ส่งข้อความ งง
-  return '',200
+    json_line = request.get_json()
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
+    user = decoded["events"][0]['replyToken']
+    #id=[d['replyToken'] for d in user][0]
+    #print(json_line)
+    print("ผู้ใช้：",user)
+    sendText(user,'งง') # ส่งข้อความ งง
+    return '',200
 
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
-#  Authorization = 'Bearer ENTER_ACCESS_TOKEN' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
+  Authorization = 'Bearer bNSZWdvzwydRYzhK14gMTbhCuYThxxXGoP+78YvKw5wOFsOiFh11bEcbJjvB7UORayyWvkZtjZKcBiqQHpr79VgdCS4UBG5m38NSiNhCX0Vw6scv/QDB5DUHo6zVhGY449Basd6Y5Em+KzasZtxErgdB04t89/1O/w1cDnyilFU=' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
   headers = {
       'Content-Type': 'application/json; charset=UTF-8',
       'Authorization':Authorization
