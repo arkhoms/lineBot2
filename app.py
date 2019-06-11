49b5c1b44818e145b0215af6640f40eae90d43ee
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-06-11 19:04:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,5 +23,17 @@ def callback():
   sendText(user,'งง') # ส่งข้อความ งง
   return '',200
 
+def sendText(user, text):
+  LINE_API = 'https://api.line.me/v2/bot/message/reply'
+#  Authorization = 'Bearer ENTER_ACCESS_TOKEN' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
+  headers = {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization':Authorization
+  }
+  data = json.dumps({
+      "replyToken":user,
+      "messages":[{"type":"text","text":text}]})
+    r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
+
 if __name__ == '__main__':
     app.run()
