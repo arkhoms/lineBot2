6bf7fe2a134033cce60b131064d9a456352d2e12
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-06-11 21:47:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def callback():
         "replyToken":user,
         "messages":[{"type":"text","text":text}]
     })
-    r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
+#    r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
     return '',200
 
 def sendText(user, text):
