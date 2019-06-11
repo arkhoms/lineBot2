500df209de0dec9fa9fd65fb9b015cd94187f36d
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-06-11 21:37:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def callback():
     #id=[d['replyToken'] for d in user][0]
     #print(json_line)
 #    print("ผู้ใช้：",user)
-#    sendText(user,'งง') 
-#    LINE_API = 'https://api.line.me/v2/bot/message/reply'
+#    sendText(user,'งง')
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
 #    Authorization = 'Bearer bNSZWdvzwydRYzhK14gMTbhCuYThxxXGoP+78YvKw5wOFsOiFh11bEcbJjvB7UORayyWvkZtjZKcBiqQHpr79VgdCS4UBG5m38NSiNhCX0Vw6scv/QDB5DUHo6zVhGY449Basd6Y5Em+KzasZtxErgdB04t89/1O/w1cDnyilFU=' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
 #    headers = {
 #        'Content-Type': 'application/json; charset=UTF-8',
