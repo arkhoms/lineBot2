edf7ce8868da04d846eb2456bf40794a30dcaeeb
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-06-11 20:31:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,15 +27,16 @@ def callback():
 
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
-  Authorization = 'Bearer bNSZWdvzwydRYzhK14gMTbhCuYThxxXGoP+78YvKw5wOFsOiFh11bEcbJjvB7UORayyWvkZtjZKcBiqQHpr79VgdCS4UBG5m38NSiNhCX0Vw6scv/QDB5DUHo6zVhGY449Basd6Y5Em+KzasZtxErgdB04t89/1O/w1cDnyilFU=' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
-  headers = {
-  'Content-Type': 'application/json; charset=UTF-8',
-  'Authorization': Authorization
-  }
-  data = json.dumps({
-  "replyToken":user,
-  "messages":[{"type":"text","text":text}]})
-  r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
+#  Authorization = 'Bearer bNSZWdvzwydRYzhK14gMTbhCuYThxxXGoP+78YvKw5wOFsOiFh11bEcbJjvB7UORayyWvkZtjZKcBiqQHpr79VgdCS4UBG5m38NSiNhCX0Vw6scv/QDB5DUHo6zVhGY449Basd6Y5Em+KzasZtxErgdB04t89/1O/w1cDnyilFU=' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
+#  headers = {
+#    'Content-Type': 'application/json; charset=UTF-8',
+#    'Authorization': Authorization
+#  }
+#  data = json.dumps({
+#    "replyToken":user,
+#    "messages":[{"type":"text","text":text}]
+#  })
+#  r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
 
 if __name__ == '__main__':
     app.run()
