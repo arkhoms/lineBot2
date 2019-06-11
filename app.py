5d5396212d1cbd0e1976ee513aaf323bc95d6611
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-06-11 18:48:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,17 @@ def webhook():
 #    if request.method === 'POST':
         return "OK"
 
+@app.route('/callback', methods=['POST'])
+def callback():
+  json_line = request.get_json()
+  json_line = json.dumps(json_line)
+  decoded = json.loads(json_line)
+  user = decoded["events"][0]['replyToken']
+  #id=[d['replyToken'] for d in user][0]
+  #print(json_line)
+  print("ผู้ใช้：",user)
+  sendText(user,'งง') # ส่งข้อความ งง
+  return '',200
+
 if __name__ == '__main__':
     app.run()
