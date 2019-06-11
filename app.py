4b7e5691d9f3092e35f56ec122f2fb46771e4afe
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-06-11 22:42:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,23 +23,12 @@ def callback():
     #id=[d['replyToken'] for d in user][0]
     #print(json_line)
 #    print("ผู้ใช้：",user)
-#    sendText(user,'งง')
-    LINE_API = 'https://api.line.me/v2/bot/message/reply'
-    Authorization = 'Bearer {bNSZWdvzwydRYzhK14gMTbhCuYThxxXGoP+78YvKw5wOFsOiFh11bEcbJjvB7UORayyWvkZtjZKcBiqQHpr79VgdCS4UBG5m38NSiNhCX0Vw6scv/QDB5DUHo6zVhGY449Basd6Y5Em+KzasZtxErgdB04t89/1O/w1cDnyilFU=}' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': Authorization
-    }
-    data = json.dumps({
-        "replyToken":user,
-        "messages":[{"type":"text","text":text}]
-    })
-    r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
+    sendText(user,'งง')
     return '',200
 
 def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
-  Authorization = 'Bearer bNSZWdvzwydRYzhK14gMTbhCuYThxxXGoP+78YvKw5wOFsOiFh11bEcbJjvB7UORayyWvkZtjZKcBiqQHpr79VgdCS4UBG5m38NSiNhCX0Vw6scv/QDB5DUHo6zVhGY449Basd6Y5Em+KzasZtxErgdB04t89/1O/w1cDnyilFU=' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
+  Authorization = 'Bearer {bNSZWdvzwydRYzhK14gMTbhCuYThxxXGoP+78YvKw5wOFsOiFh11bEcbJjvB7UORayyWvkZtjZKcBiqQHpr79VgdCS4UBG5m38NSiNhCX0Vw6scv/QDB5DUHo6zVhGY449Basd6Y5Em+KzasZtxErgdB04t89/1O/w1cDnyilFU=}' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
   headers = {
     'Content-Type': 'application/json; charset=UTF-8',
     'Authorization': Authorization
