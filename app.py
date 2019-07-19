0e0d29739ca8f78ad974ef282b28283efa1f6d2d
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-07-19 10:55:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,14 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    sendText(user,userText)
+    #sendText(user,userText)
+    if (userText == 'สวัสดี') :
+        sendText(user,'เออออว่ะ')
+    elif (userText == 'ไปล่ะ') :
+        sendText(user,'ไปไหนก็ไป ไป๊')
+    else :
+        sendText(user,'ว่าไงนะ')
+
     return '',200
 
 def sendText(user, text):
