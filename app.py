a16645bf0d5ea62f1825f371ab42875d1b157dfd
Arkhom Suwanprasert <arkhoms@gmail.com>
2020-02-21 06:54:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,8 +41,9 @@ def callback():
         for line in f.readlines():
             a = line.split(",")
             if(userAction==a[0]):
-                sendText(user,a[4])
+                nameList=nameList+", "+a[4]
         f.close()
+        sendText(user,nameList)
     except Exception:
         sendText(user,"ขออภัย..ไม่สามารถเปิดไฟล์ได้")
     
