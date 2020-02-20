31f4a9c6f10aed47e49167bd0a946bb44c7b7961
Arkhom Suwanprasert <arkhoms@gmail.com>
2020-02-21 06:22:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def callback():
 #    userText = decoded["events"][0]['message']['text']
     user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
     userText = decoded['queryResult']['intent']['displayName']
-    userAction = decoded['queryResult']['parameters']['number']
+    userAction = decoded['queryResult']['parameters']['studentId']
 #    sendText(user,"ไม่บ้าน้าาา")
     sendText(user,userText+" "+userAction)
     try:
